6bf8afdc07531a434083516e1b2e76b3197d556a
MicheleM98 <55845560+MicheleM98@users.noreply.github.com>
2023-09-20 18:03:23 +0200

    fix funzioni da modificare

--- esempio.js
+++ esempio.js
@@ -6,14 +6,31 @@ function inviaMessaggio() {
     console.log('messaggio inviato');
 }
 
+const grid = [
+    0 [item1, item2, item3, item4]
+]
+
 function caricaDati() {
     var xhttp = new XMLHttpRequest();
+    const grid = document.getElementById("grid");
     xhttp.open("GET", "https://jsonplaceholder.typicode.com/users");
     xhttp.send();
     xhttp.onload = () => {
         console.log(xhttp);
         if (xhttp.status === 200) {
-            console.log(JSON.parse(xhttp.response));
+            const parsedJSON = JSON.parse(xhttp.response)
+            parsedJSON.forEach(element => {
+                let name = `<div class="grid-item grid-item2 grid-center grid-center">` + element.name + `</div>`;
+                let email = `<div class="grid-item grid-item2 grid-center grid-center">` + element.email + `</div>`;
+                let phone = `<div class="grid-item grid-item2 grid-center grid-center">` + element.phone + `</div>`;
+                let action = `<div class="grid-item grid-item2 grid-center grid-center">
+                                <button onclick="cancellaRecord()" class="grid-link">Cancella</button>
+                            </div>`;
+                let div = document.createElement("div");
+                div.innerHTML = name + email + phone + action;
+                console.log(div);
+                document.body.appendChild(div); 
+            });
         } else {
             console.log(`error ${xhttp.status} ${xhttp.statusText}`);
         }
