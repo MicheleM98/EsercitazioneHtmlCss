5d11d1a6b13b652f84a6148d447955228618e03b
MicheleM98 <55845560+MicheleM98@users.noreply.github.com>
2023-09-24 18:07:57 +0200

    conversione da javascript a jquery

--- esempio.js
+++ esempio.js
@@ -1,85 +1,90 @@
-window.addEventListener('load', function () {
-    const form = document.getElementById('form1');
-
-    form.addEventListener('submit', (event) => {
+$(document).ready(function () {
+    $('#form1').submit(function (event) {
         event.preventDefault();
         controlloForm();
-    })
-})
+    });
+
+    $('#txtMessage').on('input', function () {
+        $('#txtMessage').css('border-color', '#ADADAD');
+    });
+});
 
 function controlloForm() {
-    const phone = document.getElementById("phone");
-    const email = document.getElementById("email");
-    const phoneValue = phone.value;
-    const emailValue = email.value;
-    let lblPhoneError = document.getElementById('phoneError');
-    let lblMailError = document.getElementById('mailError');
+    const phone = $('#phone');
+    const email = $('#email');
+    const phoneValue = phone.val();
+    const emailValue = email.val();
+    const lblPhoneError = $('#phoneError');
+    const lblMailError = $('#mailError');
 
     if (isNaN(phoneValue) || phoneValue.length < 1) {
-        phone.style.borderColor = "#FF0000";
-        lblPhoneError.setAttribute('title','TELEFONO NON VALIDO');
+        phone.css('border-color', '#FF0000');
+        lblPhoneError.attr('title', 'TELEFONO NON VALIDO');
     } else {
-        phone.style.borderColor = "#ADADAD";
-        lblPhoneError.setAttribute('title','');
-
+        phone.css('border-color', '#ADADAD');
+        lblPhoneError.attr('title', '');
     }
-    if (emailValue.indexOf("@") === -1 || emailValue.indexOf(".") === -1) {
-        email.style.borderColor = "#FF0000";
-        lblMailError.setAttribute('title','MAIL NON VALIDA');
 
+    if (emailValue.indexOf('@') === -1 || emailValue.indexOf('.') === -1) {
+        email.css('border-color', '#FF0000');
+        lblMailError.attr('title', 'MAIL NON VALIDA');
     } else {
-        email.style.borderColor = "#ADADAD";
-        lblMailError.setAttribute('title','');
+        email.css('border-color', '#ADADAD');
+        lblMailError.attr('title', '');
     }
 }
 
 function inviaMessaggio() {
-    const chat = document.getElementById("chat");
-    const text = document.getElementById("txtMessage").value;
-    const div = document.createElement('div');
-    const p = document.createElement('p');
-    p.classList.add('msgUser', 'chatMsg', 'text', 'boxMsg', 'right');
-    p.setAttribute('value', text);
-    const lbl = document.createElement('label');
-    lbl.classList.add('triangle-bottom');
-    div.appendChild(p);
-    div.appendChild(lbl);
-    console.log(div);
+    const chat = $('#chatBox');
+    const textArea = $('#txtMessage');
+    let text = textArea.val();
+    if(text !== '') {
+        const div = $('<div></div>');
+        const p = $('<p></p>').addClass('msgUser chatMsg text boxMsg right').text(text);
+        const lbl = $('<label></label>').addClass('triangle-down');
+        div.append(p);
+        div.append(lbl);
+        chat.append(div);
+        textArea.val('');
+
+        setTimeout(() => {
+            const divResponse = $('<div></div>');
+            const lblResponse = $('<label></label>').addClass('triangle-up');
+            const pResponse = $('<p></p>').addClass('msgAldo chatMsg text boxMsg left').text('Miiinchia!');
+            divResponse.append(lblResponse);
+            divResponse.append(pResponse);
+            chat.append(divResponse);
+        }, 2000);
+    }
 }
 
 async function caricaDati() {
-    const grid = document.getElementById("grid");
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const grid = $('#grid');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
-    grid.innerHTML = '';
+    grid.html('');
 
     const headings = ['Clienti', 'Email', 'Telefono', 'Azioni'];
 
     for (const heading of headings) {
-        const div = document.createElement('div');
-        div.classList.add('grid-item', 'grid-item2', 'firstRow');
-        div.innerText = heading;
+        const div = $('<div></div>').addClass('grid-item grid-item2 firstRow').text(heading);
         grid.append(div);
     }
 
     data.forEach((element, index) => {
         grid.append(...['name', 'email', 'phone'].map(item => {
-            const div = document.createElement('div');
-            div.classList.add('grid-item', 'grid-item2', 'grid-center');
-            div.innerText = element[item]
+            const div = $('<div></div>').addClass('grid-item grid-item2 grid-center').text(element[item]);
             return div;
-        }))
+        }));
 
-        const button = document.createElement('button');
-        button.classList.add('grid-link');
+        const button = $('<button></button>').addClass('grid-link').text('Cancella');
 
-        button.onclick = () => cancellaRecord(index);
+        button.click(() => cancellaRecord(index));
 
-        button.innerText = 'Cancella';
         grid.append(button);
     });
 }
 
 function cancellaRecord(index) {
-    console.log("Bottone Cancella", index)
+    console.log('Bottone Cancella', index);
 }
