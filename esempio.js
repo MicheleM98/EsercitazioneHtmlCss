021ef45a867c8b508e4aa83f9ae7de1594ebb08d
MicheleM98 <55845560+MicheleM98@users.noreply.github.com>
2023-09-27 15:52:35 +0200

    add js

---
+++ esempio.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+    $('#form').submit(function (event) {
+        event.preventDefault();
+        formValidator();
+    });
+    
+    $('#textMessage').on('input', function () {
+        $('#textMessage').css('border-color', '#ADADAD');
+    });
+});
+
+function formValidator() {
+    const phone = $('#phone');
+    const email = $('#email');
+    const phoneValue = phone.val();
+    const emailValue = email.val();
+    const labelPhoneError = $('#phoneError');
+    const labelMailError = $('#mailError');
+
+    if (isNaN(phoneValue) || phoneValue.length < 1) {
+        phone.css('border-color', '#FF0000');
+        labelPhoneError.attr('title', 'TELEFONO NON VALIDO');
+    } else {
+        phone.css('border-color', '#ADADAD');
+        labelPhoneError.attr('title', '');
+    }
+
+    if (emailValue.indexOf('@') === -1 || emailValue.indexOf('.') === -1) {
+        email.css('border-color', '#FF0000');
+        labelMailError.attr('title', 'MAIL NON VALIDA');
+    } else {
+        email.css('border-color', '#ADADAD');
+        labelMailError.attr('title', '');
+    }
+}
+
+function sendMessage() {
+    const chat = $('#chatBox');
+    const textArea = $('#textMessage');
+    let text = textArea.val();
+    if(text !== '') {
+        const div = $('<div></div>');
+        const p = $('<p></p>').addClass('sent-message message box-message right font-normal').text(text);
+        const trinagle = $('<p></p>').addClass(' triangle triangle-down');
+        div.append(p);
+        div.append(trinagle);
+        chat.append(div);
+        textArea.val('');
+
+        setTimeout(() => {
+            const divResponse = $('<div></div>');
+            const trinagleResponse = $('<p></p>').addClass('triangle triangle-up');
+            const pResponse = $('<p></p>').addClass('receved-message message box-message left font-normal').text('Miiinchia!');
+            divResponse.append(trinagleResponse);
+            divResponse.append(pResponse);
+            chat.append(divResponse);
+        }, 2000);
+    }
+}
+
+async function gridReload() {
+    const grid = $('#grid');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    grid.html('');
+
+    const headings = ['Clienti', 'Email', 'Telefono', 'Azioni'];
+
+    for (const heading of headings) {
+        const div = $('<div></div>').addClass('grid-item font-normal first-row').text(heading);
+        grid.append(div);
+    }
+
+    data.forEach((element, index) => {
+        grid.append(...['name', 'email', 'phone'].map(item => {
+            const div = $('<div></div>').addClass('grid-item font-normal grid-center').attr('data-id', element.id).text(element[item]);
+            return div;
+        }));
+        
+        const divButton = $('<div></div>').attr('data-id', element.id).addClass('grid-item font-normal grid-center');
+        const button = $('<button></button>').addClass('grid-link').text('Cancella');
+        divButton.append(button);
+        button.click(() => removeRecord(element.id));
+
+        grid.append(divButton);
+    });
+}
+
+function removeRecord(id) {
+    $(`.grid-center[data-id="${id}"]`).remove();
+}
